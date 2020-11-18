f392e5451e8fc9a62ae08d68c98ced463dd3b8e2
Warness <65484013+Warness@users.noreply.github.com>
2020-11-18 23:51:42 +0200

    Исправление конфига, работа с логикой запросов страниц

--- src/index/index.js
+++ src/index/index.js
@@ -1,12 +1,2 @@
 import '../scss/main.scss'
-import request from '../js/apiRequest.js';
-
-console.log('number 1');
-
-// request.getTrendFilms();
-// request.setPage(25);
-// request.getTrendFilms();
-// request.searchFilms("matrix")
-// request.getFilmById(26214)
-
-//
+import '../js/main-page-gallery'
