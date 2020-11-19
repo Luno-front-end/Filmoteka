a9a9bf66691b16602d906333272648c991fe5aca
TOR968 <tor968@yahoo.com>
2020-11-19 21:48:49 +0200

    Update myLibrary.js

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -1,44 +1,32 @@
 import request from '../js/apiRequest';
-import cards from "../Templates/myLibrary.hbs";
+import cards from "../Templates/myLibrary.hbs"; // заглушка
 
-localStorage.setItem('queue', '[625512,612568,625128]')
-localStorage.setItem('watched', '[625312]')
+// localStorage.setItem('queue', '[625512,612568,625128]')
+// localStorage.setItem('watched', '[625312]')
 
 const btnWatched = document.querySelector('.btn-watched'); // класс кнопки
 const btnQueue = document.querySelector('.btn-queue'); // класс кнопки
 
-const libraryContainer = document.querySelector(".js-menu"); // js-menu замінить на почактовий класс розмітки
+const libraryContainer = document.querySelector(".gallery"); // js-menu замінить на почактовий класс розмітки
 
 
 class ButtonsLibrary {
   constructor(btnName) {
     this.btnName = btnName;
     this.response = JSON.parse(localStorage.getItem(this.btnName))
-    console.log('inStorage', this.response)
-    console.log('inStoragelength', this.response.length)
   }
-  onClick() {
-      this.response.map(e => {
-      return request.getFilmById(e)
+  cardData() {
+    this.response.map(id => {
+                 return request.getFilmById(id).then(data => {
+    const markup = cards(data.results);
+    libraryContainer.innerHTML = markup;
+          });
       })
-   
-  // this.cardsMarkup()
-
-}
-  //  cardsMarkup() {
-  // //   libraryContainer.insertAdjacentHTML("beforeend", createMarkup(data));// json??
-  // //   return cards(data.results)
-
-  //    this.onClick().then(data => {
-  //      console.dir(data);
-  //   const markup = cards(data.results);
-  //   libraryContainer.innerHTML = markup;
-  // });
-  // }
+    }
 }
 
 const watchedClick = new ButtonsLibrary('watched')
-btnWatched.addEventListener('click', watchedClick.onClick.bind(watchedClick));
+btnWatched.addEventListener('click', watchedClick.cardData.bind(watchedClick));
 
 const queueClick = new ButtonsLibrary('queue')
-btnQueue.addEventListener('click', queueClick.onClick.bind(queueClick));
+btnQueue.addEventListener('click', queueClick.cardData.bind(queueClick));
