357f3255bbf6b150550c44fd40872583e863a856
Maxim <Tor968@yahoo.com>
2020-11-19 16:34:09 +0200

    Update myLibrary.js

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -1,6 +1,8 @@
 import request from '../js/apiRequest';
-import cards from "../tools/myLibrary.hbs";
+import cards from "../Templates/myLibrary.hbs";
 
+localStorage.setItem('queue', '[625512,612568,625128]')
+localStorage.setItem('watched', '[625312]')
 
 const btnWatched = document.querySelector('.btn-watched'); // класс кнопки
 const btnQueue = document.querySelector('.btn-queue'); // класс кнопки
@@ -11,20 +13,32 @@ const libraryContainer = document.querySelector(".js-menu"); // js-menu замі
 class ButtonsLibrary {
   constructor(btnName) {
     this.btnName = btnName;
-      this.object = JSON.parse(localStorage.getItem(this.btnName))
+    this.response = JSON.parse(localStorage.getItem(this.btnName))
+    console.log('inStorage', this.response)
+    console.log('inStoragelength', this.response.length)
   }
   onClick() {
-    this.Object.map(e => {
+      this.response.map(e => {
       return request.getFilmById(e)
-    });
-  this.cardsMarkup()
-  }
-  cardsMarkup() {
-    libraryContainer.insertAdjacentHTML("beforeend", createMarkup(object));// json??
-    return cards(object)
-  }
+      })
+   
+  // this.cardsMarkup()
+
 }
-const watched = new ButtonsLibrary('watched')
-btnWatched.addEventListener('click', watched.onClick.bind(watched));
-const queue = new ButtonsLibrary('queue')
-btnQueue.addEventListener('click', queue.onClick.bind(queue));
+  //  cardsMarkup() {
+  // //   libraryContainer.insertAdjacentHTML("beforeend", createMarkup(data));// json??
+  // //   return cards(data.results)
+
+  //    this.onClick().then(data => {
+  //      console.dir(data);
+  //   const markup = cards(data.results);
+  //   libraryContainer.innerHTML = markup;
+  // });
+  // }
+}
+
+const watchedClick = new ButtonsLibrary('watched')
+btnWatched.addEventListener('click', watchedClick.onClick.bind(watchedClick));
+
+const queueClick = new ButtonsLibrary('queue')
+btnQueue.addEventListener('click', queueClick.onClick.bind(queueClick));
