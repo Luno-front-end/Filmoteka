236223c50e19edddedc32d0ddad33d5b8b2ae07c
TOR968 <tor968@yahoo.com>
2020-11-19 22:52:28 +0200

    add debounce

--- src/js/movieSearch.js
+++ src/js/movieSearch.js
@@ -7,7 +7,9 @@ const input = document.querySelector('input');
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery'); // замінить клас
 
-form.addEventListener('submit', renderImages);
+const debounce = require('lodash.debounce');
+
+form.addEventListener('submit', debounce(renderImages, 500));
 
 function renderImages(e) {
   e.preventDefault();
