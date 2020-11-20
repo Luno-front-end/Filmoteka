5e52e84ea09f15993a494dbe28a22826a69fe419
Maxim <Tor968@yahoo.com>
2020-11-20 14:49:10 +0200

    Update movieSearch.js

--- src/js/movieSearch.js
+++ src/js/movieSearch.js
@@ -1,33 +1,11 @@
 import request from '../js/apiRequest';
-import card from '../Templates/imageCard.hbs';
 import debounce from 'lodash.debounce';
 import createGallery from './trendFilms';
-// import '../tools/styles.css';
 
-let query = '';
 const input = document.querySelector('input');
-const form = document.querySelector('#search-form');
-const galleryList = document.querySelector('.gallery-list'); // замінить клас
+const galleryList = document.querySelector('.gallery-list');
 
-// <<<<<<< HEAD
-// const debounce = require('lodash.debounce');
-
-// form.addEventListener('submit', debounce(renderImages, 500));
-// =======
 searchFilms();
-// >>>>>>> preDev
-
-// form.addEventListener('submit', renderImages);
-// function renderImages(e) {
-//   e.preventDefault();
-//   gallery.innerHTML = '';
-//   query = input.value;
-//   request.searchFilms(query).then(data => {
-//     const markup = card(data.results);
-//     gallery.innerHTML = markup;
-//     form.reset();
-//   });
-// }
 
 function searchFilms() {
   input.addEventListener(
