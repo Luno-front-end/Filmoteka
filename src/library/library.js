1e753524a6f0e03cec5e96d005843115927e34f8
Warness <65484013+Warness@users.noreply.github.com>
2020-11-20 23:57:24 +0200

    Добавлено сообщение при пустой колекции

--- src/library/library.js
+++ src/library/library.js
@@ -1,1 +1,15 @@
 import '../sass/main.scss';
+
+
+const emptyNotice = document.querySelector('.empty-collection');
+const bodyref = document.querySelector('.gallery-list');
+
+
+// =============================
+// подставить в логику рендера колекции
+if(bodyref.length === 0){
+    emptyNotice.classList.add('is-hidden')
+}
+else{
+    emptyNotice.classList.remove("is-hidden")
+}
