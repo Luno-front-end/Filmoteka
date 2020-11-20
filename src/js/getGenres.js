56764044e129553110a2ba7060850831a8766fda
Volodymyr Kornieiev <Volydemar2032@gmail.com>
2020-11-20 14:12:14 +0200

    Исправлен поиск, добавлен depbounce, добавлена запасная картинка, если нету основной

--- src/js/getGenres.js
+++ src/js/getGenres.js
@@ -1,5 +1,4 @@
-import request from './apiRequest';
-// import genresList from '../genresList.json';
+import genresList from '../genresList.json';
 // console.log('genresList', genresList);
 // let genresIds = [];
 
@@ -14,39 +13,13 @@ import request from './apiRequest';
 // ====================================
 //  Функция подмены массива из чисел в значение
 
-function switchGenresList(database, array) {
+function switchGenresList(numbersArray = genresList, stringArray) {
   const newArr = [];
-  database.map(e => {
-    if (array.includes(e.id)) {
+  numbersArray.map(e => {
+    if (stringArray.includes(e.id)) {
       newArr.push(e.name);
     }
   });
   return newArr;
 }
-
 export default switchGenresList;
-//     await request.getTrendFilms().then(data => {
-//         console.log(data.results);
-
-//         data.results.forEach((e) => {
-//             console.log(e.genre_ids);
-
-//             // switchGenresList(e.genre_ids).then(arr => {
-//             //     console.log("arr" , arr);
-
-//             // })
-//         })
-//     }
-//    .then(data => console.log('data', data)) // финишь
-// switchGenresList(arr)
-// console.log(switchGenresList(arr));
-// export { switchGenresList }
-
-// Получить Список жанров
-
-// request.getTrendFilms().then(({data: {results: genres}}) => console.log("filmGenres", genres));
-
-// Создать пустой массив
-// Перебрать список жанров, при совпадении занести значение в массив
-// вернуть массив елементов
-// вставить массив елементов в карточку
