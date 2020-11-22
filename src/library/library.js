5949650e7cf6c1ee490f28d559f62c729df991e7
TOR968 <tor968@yahoo.com>
2020-11-22 18:47:48 +0200

    delete  commit

--- src/library/library.js
+++ src/library/library.js
@@ -7,18 +7,6 @@ import '../js/team'
 
 
 
-// // import  request from "../js/apiRequest"
-// // import AddLocalStorage from '../js/AddLocalStorage'
-
-// // const queue = new AddLocalStorage('queue', id, refs.queue, 'asssss')
-// // refs.queue.addEventListener('click', queue.addLocalStorage.bind(queue))
-
-// // const watched = new AddLocalStorage('watched', id, refs.watched, 'asssss', queue)
-// // refs.watched.addEventListener('click', watched.addLocalStorage.bind(watched))
-
-
-
-// // подключить пагинацию 
 
 
 
